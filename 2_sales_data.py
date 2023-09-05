e5e05a0ac73be22a5455a0014d91938302a2975c
Sudharsanan v <112715541+GokulshanthoshM@users.noreply.github.com>
2023-09-05 08:54:21 +0530

    delete

--- 2_sales_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-data1=pd.read_csv("D:\Fundamentals of data science\supermarket_sales - Sheet1.csv")
-print(data1)
-data2=np.array(data1)[:,9]
-print(data2)
-avg1=sum(data2)/len(data2)
-print(f"average of sales is {avg1}")
